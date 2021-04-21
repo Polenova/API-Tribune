193f89c3df7e374e693dff66caf8dec0a8402c70
Marina Polenova <m.v.polenova@gmail.com>
2021-04-21 13:21:13 +0200

    fix readOnly

--- src/main/kotlin/ru.polenova/dto/UserResponseDto.kt
+++ src/main/kotlin/ru.polenova/dto/UserResponseDto.kt
@@ -23,7 +23,7 @@ data class UserResponseDto(
                 attachmentImage = user.attachmentImage,
                 status = userService.checkStatus(user.idUser),
                 token = user.token,
-                readOnly = userService.checkReadOnly(idUser, postService)
+                readOnly = userService.checkReadOnly(user.idUser, postService)
             )
         }
     }
